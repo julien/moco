bf39530edb6dcf04cf95db8b01e91af267ba30ad
Julien Castelain <jcastelain@gmail.com>
2015-01-12 21:19:23 +0100

    tests(*): increase coverage

--- main_test.go
+++ main_test.go
@@ -1,15 +1,38 @@
 package main
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestGetFile(t *testing.T) {
 
 	f, e := getFile("./main.go")
 	if e != nil {
-		t.Errorf("got %v", e)
+		t.Errorf("got error %v", e)
 	}
 
 	if f.Name() != "./main.go" {
-		t.Errorf("got %v want \"./main.go\"", f.Name())
+		t.Errorf("got %v want ./main.go", f.Name())
+	}
+
+	// Test for a non existing file
+	f, e = getFile("./nofile")
+	if e == nil {
+		t.Errorf("expected error, go %v", f)
+	}
+}
+
+func TestReadFile(t *testing.T) {
+	f, e := getFile("./fixtures.txt")
+	if e != nil {
+		t.Errorf("got error %v", e)
+	}
+
+	s := readFile(f)
+	r := strings.Join(s, "")
+
+	if r != "This is a file" {
+		t.Errorf("got %v want This is a test", r)
 	}
 }
